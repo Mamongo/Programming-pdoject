47f94352e36085baa690439f75d7a878dd27da5b
Mamongo <115404407+Mamongo@users.noreply.github.com>
2024-02-12 22:04:24 +0100

    Add files via upload

---
+++ PROG proj 1.2/Web/static/src/Data.js
@@ -0,0 +1,58 @@
+let shopItemsData = [
+  {
+    id: "jadfgh",
+    name: "T Shirt",
+    price: 10,
+    desc: "White T-shirt with print.",
+    img: "/static/images/img1.jpg",
+  },
+  {
+    id: "ryukdtf",
+    name: "T Shirt",
+    price: 10,
+    desc: "White T-shirt.",
+    img: "/static/images/img2.jpg",
+  },
+  {
+    id: "dtyjkdfg",
+    name: "T Shirt",
+    price: 10,
+    desc: "Black T-shirt with text.",
+    img: "/static/images/img3.jpg",
+  },
+  {
+    id: "rtykjjfghj",
+    name: "T Shirt",
+    price: 10,
+    desc: "White T-shirt with print.",
+    img: "/static/images/img4.jpg",
+  },
+  {
+    id: "ryukerj",
+    name: "T Shirt",
+    price: 10,
+    desc: "White T-shirt with text.",
+    img: "/static/images/img5.jpg",
+  },
+  {
+    id: "rtyuifryj",
+    name: "T Shirt",
+    price: 10,
+    desc: "White T-shirt with text.",
+    img: "/static/images/img6.jpg",
+  },
+  {
+    id: "etyikortyj",
+    name: "T Shirt",
+    price: 10,
+    desc: "Pink T-shirt with print.",
+    img: "/static/images/img7.jpg",
+  },
+  {
+    id: "theytkyfhcbcv",
+    name: "T Shirt",
+    price: 10,
+    desc: "Black T-shirt.",
+    img: "/static/images/img8.jpg",
+  },
+];
